17ade0be628085ddafef773975b03635ffad597b
maksim.trofimov <makx.work@gmail.com>
2024-10-21 19:49:25 +0300

    add bin_search_rec

--- src/1.binary_search/binary_search.cpp
+++ src/1.binary_search/binary_search.cpp
@@ -5,15 +5,21 @@
 
 using namespace std;
 
-int bin_search(const vector<int> &a, const int target) {
-    int l = 0, r = a.size() - 1;
+int bin_search_rec(const vector<int> &nums, const int target, const int lo, const int hi) {
+    if (lo > hi) return -1;
+    const int mi = lo + (hi - lo) / 2;
+    if (nums[mi] == target) return mi;
+    if (nums[mi] < target) return bin_search_rec(nums, target, mi + 1, hi);
+    return bin_search_rec(nums, target, lo, mi - 1);
+}
+
+int bin_search(const vector<int> &nums, const int target) {
+    int l = 0, r = nums.size() - 1;
 
     while(l <= r) {
         int m = l + (r - 1) / 2;
-
-        if (a[m] == target) return m;
-
-        if (a[m] < target)
+        if (nums[m] == target) return m;
+        if (nums[m] < target)
             l = m + 1;
         else
             r = m - 1;
@@ -26,6 +32,9 @@ int main() {
     assert(bin_search({1, 3, 12, 545, 1230}, 12) == 2);
     assert(bin_search({1, 3, 12, 545, 1230}, 545) == 3);
     assert(bin_search({1, 3, 12, 545, 1230}, 120) == -1);
+
+    assert(bin_search_rec({1,2,3,4,5,6,7,8,9}, 4, 0, 8) == 3);
+    assert(bin_search_rec({1,2,3,4,5,6,7,8,9}, 10, 0, 8) == -1);
 
     return 0;
 }
